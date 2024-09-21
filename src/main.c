700adf60ed0ff1096f2cbf1cb821d3cc7f829796
ocirnexam <kontakt.maxmueller@outlook.com>
2024-09-21 11:10:29 +0200

    Updated database location

--- src/main.c
+++ src/main.c
@@ -5,7 +5,7 @@ int main(int argc, char* argv[])
 {
         Note note = {.id = 1, .title = "Note 1", .content = "A fresh note stored in db"};
         Note* note2;
-        if(!create_database("notes.db")) {
+        if(!create_database("/usr/share/notes/notes.db")) {
                 insert_note(note);
                 note2 = get_note(1);
                 if (note2) {
